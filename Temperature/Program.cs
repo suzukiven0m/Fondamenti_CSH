20c371c24e9f5c6518f2c86bcc122514a4610bad
suzukiven0m <facundo.fanti@gmail.com>
2026-02-20 16:17:56 +0100

    finished excercises

---
+++ Temperature/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Temperature
+{
+    internal class Program
+    {
+        //Chiedi all'utente di inserire una temperatura in Celsius. Convertila in Fahrenheit usando la
+        //formula: F = (C × 9/5) + 32. Stampa il risultato.Esempio: 100°C = 212°F
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello... Please insert a temperature in Celsius: ");
+            int temperature = int.Parse(Console.ReadLine()); 
+            int temperature_fahreinhet = (temperature * 9 / 5) + 32;
+            Console.WriteLine($"The temperature in Fahrenheit is: {temperature_fahreinhet}");
+
+        }
+    }
+}
